3ae535f860db950745ebb1b1a0087a47f1e2b6c3
BLACKIVYclub <blkivyclub@gmail.com>
2025-02-06 22:30:20 -0500

    fixes

--- src/components/ui/Button/button.tsx
+++ src/components/ui/Button/button.tsx
@@ -15,7 +15,7 @@ export const Button: React.FC<ButtonProps> = ({
     disabled = false,
     className = '',
 }) => {
-    const baseStyles = 'px-4 py-2 rounded-[5rem] py- border-2 border-black px-16 text-center transition-all duration-200';
+    const baseStyles = 'py-2 rounded-[5rem] border-2 border-black px-16 text-center transition-all duration-200';
     const variants: Record<'primary' | 'outline', string> = {
         primary: 'bg-[#f14421] text-white hover:bg-blue-600',
         outline: 'border border-black text-white hover:bg-gray-100',
